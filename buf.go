96a44f9b418dff58ca6f2ad1485f6ecb7d19caf0
秋来冬风 <2645477756@qq.com>
2023-02-26 22:19:25 +0800

    秋来冬风：修复数据竞争漏洞

--- buf.go
+++ buf.go
@@ -2,6 +2,7 @@ package cachefs
 
 import (
 	"io"
+	"sync/atomic"
 )
 
 var _ io.Reader = (*Buf)(nil)
@@ -9,15 +10,15 @@ var _ io.Reader = (*Buf)(nil)
 // Buf 循环缓存
 // 当到末尾是不会返回 [io.EOF] ,会下次从头读取
 type Buf struct {
-	i      int
-	buflen int
+	i      atomic.Int32
+	buflen int32
 	buf    []byte
 }
 
 // NewBuf 创建循环缓存
 func NewBuf(buf []byte) *Buf {
 	ret := &Buf{buf: buf}
-	ret.buflen = len(buf)
+	ret.buflen = int32(len(buf))
 	return ret
 }
 
@@ -28,29 +29,30 @@ func (buf *Buf) Read(p []byte) (n int, err error) {
 		if len(p) == 0 {
 			return 0, nil
 		}
-		buf.i = 0
+		buf.i.Store(0)
 		return 0, io.EOF
 	}
-	n = copy(p, buf.buf[buf.i:])
-	buf.i += n
+	lp := int32(len(p))
+	ni := buf.i.Add(lp)
+	n = copy(p, buf.buf[ni-lp:])
 	return n, nil
 }
 
 // Seek 实现io.Seeker接口
 func (buf *Buf) Seek(offset int64, whence int) (int64, error) {
-	off := int(offset)
+	off := int32(offset)
 	switch whence {
 	case io.SeekStart:
-		buf.i = off
+		buf.i.Store(off)
 	case io.SeekCurrent:
-		buf.i += off
+		buf.i.Add(off)
 	case io.SeekEnd:
-		buf.i = buf.buflen - off
+		buf.i.Store(buf.buflen - off)
 	}
-	return int64(buf.i), nil
+	return int64(buf.i.Load()), nil
 }
 
 // Empty 判断缓存是否到末尾
 func (buf *Buf) Empty() bool {
-	return buf.i >= buf.buflen
+	return buf.i.Load() >= buf.buflen
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module gitee.com/qiulaidongfeng/cachefs
 
-go 1.16
+go 1.19
