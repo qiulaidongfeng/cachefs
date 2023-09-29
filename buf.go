54fa1dcb6b30c16dfe26d25b0634d33a7778110a
qiulaidongfeng <2645477756@qq.com>
2023-09-29 20:13:55 +0800

    秋来冬风：删除Buf的buflen字段

--- buf.go
+++ buf.go
@@ -8,14 +8,13 @@ var _ io.Reader = (*Buf)(nil)
 
 // Buf 循环缓存
 type Buf struct {
-	buflen int
-	i      int
-	buf    []byte
+	i   int
+	buf []byte
 }
 
 // NewBuf 创建循环缓存
 func NewBuf(buf []byte) Buf {
-	return Buf{buf: buf, buflen: len(buf)}
+	return Buf{buf: buf}
 }
 
 // Read 实现 [io.Reader] 接口,读取缓存内容
@@ -42,14 +41,14 @@ func (buf *Buf) Seek(offset int64, whence int) (int64, error) {
 	case io.SeekCurrent:
 		buf.i += off
 	case io.SeekEnd:
-		buf.i = buf.buflen - off
+		buf.i = len(buf.buf) - off
 	}
 	return int64(buf.i), nil
 }
 
 // Empty 判断缓存是否到末尾
 func (buf *Buf) Empty() bool {
-	return buf.i >= buf.buflen
+	return buf.i >= len(buf.buf)
 }
 
 // Copy 浅拷贝一个循环缓存
